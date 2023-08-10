ea7a618df6c50339ab69933cc952e1abd67baaaa
Fernando Vargas <fvr950311@gmail.com>
2023-08-09 23:22:19 -0600

    Pruebas unitarias con mockito

--- src/main/java/com/example/rest_api_springboot/services/GrupoService.java
+++ src/main/java/com/example/rest_api_springboot/services/GrupoService.java
@@ -3,18 +3,19 @@ package com.example.rest_api_springboot.services;
 import com.example.rest_api_springboot.controller.handlers.GrupoNotFoundException;
 import com.example.rest_api_springboot.model.Estudiante;
 import com.example.rest_api_springboot.model.Grupo;
+import com.example.rest_api_springboot.model.Materia;
 import com.example.rest_api_springboot.persistence.GrupoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
-public class GrupoService {
+public class EstudianteService {
     @Autowired //Injectar las dependencias necesarias
-    private final GrupoRepository grupoRepository;
+    GrupoRepository grupoRepository;
 
     public List<Grupo> obtenGrupos(){
         return grupoRepository.findAll();
@@ -26,8 +27,8 @@ public class GrupoService {
         return grupoRepository.findByActivo(status);
     }
 
-    public Grupo obtenGrupo(long id) {
-        return grupoRepository.findById(id).orElseThrow(() -> new GrupoNotFoundException(id));
+    public Optional<Grupo> obtenGrupo(long id) {
+        return grupoRepository.findById(id);
     }
 
     public Grupo guardaGrupo(Grupo newGrupo) {
